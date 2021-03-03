e642ca70ddb05c5d5f7b202df87db965baef425b
Pierre Bedell <pierre.bedell@gmail.com>
2021-03-03 21:22:24 +0100

    object placement on polygon sides wip - dirty chamfer method removed

--- flexi_box.lua
+++ flexi_box.lua
@@ -35,19 +35,6 @@ svg_qrcode = svg_contouring(Path .. 'qrcode.svg',90)
 
 --####################################################################
 
-
-function gen_angle_chamfer(nb_faces,diameter,cutting_width,cutting_angle)
-  local side = 2*(diameter/2)*sin(180/nb_faces)
-  local angle = 360/nb_faces
-  cut = {}
-  for i = 1, nb_faces do
-    cut[i] = rotate(0,0,i*angle)*translate(0,(diameter/2),0)*rotate(cutting_angle,0,0)*cube(side,cutting_width,cutting_width)
-  end
-  cut = union(cut)
-  cut = rotate(0,0,180-angle/2)*cut -- correcting angle to match gen_polygon()
-  return cut
-end
-
 --####################################################################
 
 lock_meat = difference{
@@ -70,8 +57,7 @@ icesl_qrcode = rotate(-90,0,0)*union(icesl_qrcode)
 box = union{
   difference{
     gen_polygon(box_nb_faces,box_diameter,box_height),
-    translate(0,0,box_wall_th)*gen_polygon(box_nb_faces,box_diameter-box_wall_th*2,box_height),
-    translate(0,0,-25)*gen_angle_chamfer(box_nb_faces,box_diameter,30,45) -- angles chamfer
+    translate(0,0,box_wall_th)*gen_polygon(box_nb_faces,box_diameter-box_wall_th*2,box_height)
   },
   translate(0,0,box_height-lid_height)*lock_meat
 }
@@ -84,7 +70,6 @@ lid = difference{
     gen_polygon(box_nb_faces,(box_diameter-box_wall_th*4)-lid_clearance*2,lid_height),
     translate(0,0,lid_height)*gen_polygon(box_nb_faces,box_diameter,box_wall_th),
   },
-  translate(0,0,33)*gen_angle_chamfer(box_nb_faces,box_diameter,30,45), -- angles chamfer
   translate(0,0,box_wall_th/2)*lid_cavity,
   cylinder(key_hole_diameter/2,lid_height+box_wall_th)
 }
--- poly_shapes.lua
+++ poly_shapes.lua
@@ -1,4 +1,4 @@
-function gen_polygon(nb_faces,diameter,height)
+function gen_polygon(nb_faces, diameter, height)
   local a = 360/nb_faces
   local r = diameter/2
   
@@ -10,7 +10,51 @@ function gen_polygon(nb_faces,diameter,height)
   return linear_extrude(v(0,0,height),poly)
 end
 
-function gen_trapeze(base1,base2,height,thickness)
+function get_polygon_side(polygon_nb_faces, polygon_diameter)
+	return 2*(polygon_diameter/2)*sin(180/polygon_nb_faces)
+end
+
+function place_on_polygon(polygon_nb_faces, polygon_diameter, element_id, element_type, shape)
+	-- the element_id is determined counter-clockwise, 
+	-- with the first face being the on bottom-right of the polygon
+	-- (and the first corner on the bottom)
+	--	
+	--							 		top (x)
+	--		  						  *
+	--							3	  *   * 	2
+	--								*       *
+	--	left(-y)	4 	*       * 	1
+	--								*       *
+	--		  				5	  *   * 	0	<- first side
+	--		    						*
+	--
+	-- element_type refers to where the shape is placed:
+	--						- 1/true: on the face
+	--						- 0/false: on the corner
+	if element_type == 1 then element_type = true else element_type = false end
+	local a = 360/polygon_nb_faces
+	local angle_offset = 0
+	local r = polygon_diameter/2
+	if element_type then
+		angle_offset = a/2
+	end
+	local position = v(r*sin(angle_offset+a*element_id),-r*cos(angle_offset+a*element_id)) -- TODO: revise position to eliminate offset due to rotation
+
+	return translate(position)*rotate(0,0,0)*shape
+end
+
+function place_on_all(polygon_nb_faces, polygon_diameter, element_type,shape)
+	-- element_type refers to where the shape is placed:
+	--						- 1/true: on the face
+	--						- 0/false: on the corner
+	shapes = {}
+	for i = 0, polygon_nb_faces-1 do
+		shapes[i] = place_on_polygon(polygon_nb_faces,polygon_diameter,i,element_type,shape)
+	end
+	return union(shapes)
+end
+
+function gen_trapeze(base1, base2, height, thickness)
 	local trapeze  = {
 		v(-base1/2,0),
 		v(base1/2,0),
